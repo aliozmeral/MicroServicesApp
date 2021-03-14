7f14a2c4a302ad7db8771dbc4bd9f3bbade1fd68
Ali Özmeral <aliozmeral@gmail.com>
2021-03-14 14:11:10 +0300

    Kişiler ve İletişim Bilgileri Entity leri eklendi.

---
+++ src/Contact/Contact.API/Entities/IletisimBilgileri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contact.API.Entities
+{
+    public class IletisimBilgileri
+    {
+        public string Id { get; set; }
+        public string BilgiTipi { get; set; }
+
+        public string BilgiIcerik { get; set; }
+
+    }
+}
---
+++ src/Contact/Contact.API/Entities/Kisiler.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contact.API.Entities
+{
+    public class Kisiler
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string UUID { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Firma { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public List<string> Iletisim { get; set; }
+
+    }
+}
